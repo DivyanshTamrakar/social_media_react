4b2c674387e9bcbdde10f07fb3484608096f185a
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-01-24 01:49:01 +0530

    remove un-necessary css

--- FrontEnd/src/Components/Post/Post.style.js
+++ FrontEnd/src/Components/Post/Post.style.js
@@ -30,9 +30,6 @@ export const RightSection = styled.div`
   flex-direction: column;
   height: max-content;
   margin-left: 200px;
-  /* box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  background-color: white;
-  padding: 10px; */
 `;
 
 export const HeadArea = styled.div`
