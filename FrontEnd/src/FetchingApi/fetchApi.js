104ed50594fdbd1c53bd97ca6d94984ed626f004
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-02-19 14:57:44 +0530

    refactor : change base URL

--- FrontEnd/src/FetchingApi/fetchApi.js
+++ FrontEnd/src/FetchingApi/fetchApi.js
@@ -28,7 +28,7 @@ export async function postData(body, ...endpoints) {
 }
 
 // export const BASE_URL = "http://localhost:5000";
-// export const BASE_URL = "https://ancient-earth-80509.herokuapp.com";
+export const BASE_URL = "https://ancient-earth-80509.herokuapp.com";
 /* This function takes query as a param and returns the array of beer objects.*/
 export const fetchSearchResults = async (query) => {
   if (query && query.length > 0) {
